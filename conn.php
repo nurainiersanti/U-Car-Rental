1b19afa565109850d68cd5142784a6155bae28e7
nurainiersanti <43580685+nurainiersanti@users.noreply.github.com>
2018-12-17 00:43:15 +0700

    Add files via upload

--- conn.php
+++ conn.php
@@ -1,10 +1,19 @@
-<?php
+<?php 
+
+function connection() {
+   // membuat konekesi ke database system
    $hostname  = "localhost";
    $username  = "root";
    $password  = "";
    $dbname  = "rentalmobil";
-   $conn = new mysqli($hostname, $username, $password, $dbname);
 
-   $dir_gambar = 'C:\xampp\htdocs\U-Car-Rental\img\\'; //direktori dalam server
-    $url_folder_gambar = 'http://localhost/U-Car-Rental/img/';  //URL dari direktori jika diakses lewat browser
-?>  
+   $conn = new mysqli($hostname, $username, $password);
+
+   if(! $conn) {
+	die('Koneksi gagal: ' . mysqli_error());
+   }
+   //memilih database yang akan dipakai
+   mysqli_select_db($conn,$dbname);
+	
+   return $conn;
+}
