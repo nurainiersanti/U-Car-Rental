7eb1d4b45927587856d9a790a4984ae327e3bbeb
jihanabilah@gmail.com <jihanabilah@gmail.com>
2018-12-16 23:29:51 +0700

    jihan

--- conn.php
+++ conn.php
@@ -5,8 +5,6 @@
    $dbname  = "rentalmobil";
    $conn = new mysqli($hostname, $username, $password, $dbname);
 
-   $dir_gambar = 'C:\xampp\htdocs\U-Car-Rental\ktp\\'; //direktori dalam server
-    $url_folder_gambar = 'http://localhost/U-Car-Rental/ktp/';  //URL dari direktori jika diakses lewat browser
-    $dir_gambar = 'C:\xampp\htdocs\U-Car-Rental\sim\\'; //direktori dalam server
-    $url_folder_gambar = 'http://localhost/U-Car-Rental/sim/';  //URL dari direktori jika diakses lewat browser
+   $dir_gambar = 'C:\xampp\htdocs\U-Car-Rental\img\\'; //direktori dalam server
+    $url_folder_gambar = 'http://localhost/U-Car-Rental/img/';  //URL dari direktori jika diakses lewat browser
 ?>  
