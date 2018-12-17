6f394a08ecc34426081a543f0bcbb9a54be93888
ardi <dwidarmaganteng@gmail.com>
2018-12-17 14:07:04 +0700

    gapake input

--- admin/index.php
+++ admin/index.php
@@ -33,9 +33,6 @@
                 <a class="nav-link active" href="<?php echo "index.php"; ?>">Data Order Pelanggan</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="<?php echo "form.php"; ?>">Input Data Order Pelanggan</a>
-              </li>
-              <li class="nav-item">
                 <a class="nav-link" href="<?php echo "logout.php"; ?>">Logout</a>
               </li>
             </ul>
